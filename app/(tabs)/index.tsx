8f6e6622efdbe279c0459c0cbe6e7d0d8e22c6ac
fhub-1 <flutterhub1.21.0@gmail.com>
2023-10-15 21:35:10 +0300

    pushing my fist branche code on the app looking so dope

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,41 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
+import { Alert, Pressable, StyleSheet, Text } from "react-native";
+import { View } from "../../components/Themed";
+// import { Agenda, AgendaEntry } from "react-native-calendars";
+import { Agenda, AgendaEntry, AgendaList } from "react-native-calendars";
+import events from "../../assets/data/events.json";
+import React from "react";
 
 export default function TabOneScreen() {
+  const renderItem = (reservation: AgendaEntry, isFirst: boolean) => {
+    const fontSize = isFirst ? 16 : 14;
+    const color = isFirst ? "black" : "#43515c";
+
+    return (
+      <Pressable
+        style={[styles.item, { height: reservation.height }]}
+        onPress={() => Alert.alert(reservation.name)}
+      >
+        <Text style={{ fontSize, color }}>{reservation.name}</Text>
+      </Pressable>
+    );
+  };
+
+  const renderEmptyDate = () => {
+    return (
+      <View style={styles.emptyDate}>
+        <Text>No events for this date. Enjoy your day!</Text>
+      </View>
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Agenda
+        items={events}
+        renderItem={renderItem}
+        renderEmptyData={renderEmptyDate}
+        // showOnlySelectedDayItems when showing only selcted date based on booking event
+      />
     </View>
   );
 }
@@ -16,16 +43,27 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
+  item: {
+    backgroundColor: "white",
+    flex: 1,
+    borderRadius: 10,
+    padding: 10,
+    marginRight: 10,
+    marginTop: 17,
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  emptyDate: {
+    height: 15,
+    flex: 1,
+    paddingTop: 30,
+    margin: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    fontSize: 16,
+    fontWeight: "900",
+    fontVariant: ["small-caps"],
+    borderRadius: 10,
+    backgroundColor: "#f0f0f0",
+    color: "#555",
   },
 });
